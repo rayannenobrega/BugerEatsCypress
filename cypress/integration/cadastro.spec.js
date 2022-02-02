9a17148446b49a8f0d009b96d1f1426e5e412fb2
rayannenobrega <nanyz.nobrega@gmail.com>
2022-02-02 11:18:52 -0300

    preenchendo campos

---
+++ cypress/integration/cadastro.spec.js
@@ -0,0 +1,23 @@
+
+describe('Cadastro', ()=>{
+    it('Usuário deve se tornar um entregador', ()=>{
+        cy.viewport(1440, 900)
+        cy.visit('https://buger-eats.vercel.app')
+        
+        cy.get('a[href="/deliver"]').click()
+        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
+
+        var entregador = {
+            nome: 'Rayanne Nobrega',
+            cpf: '00011122233',
+            email:'rayanne@hotmail.com',
+            whatsapp:'83999999999'
+        }
+
+        cy.get('input[name="name"]').type(entregador.nome)
+        cy.get('input[name="cpf"]').type(entregador.cpf)
+        cy.get('input[name="email"]').type(entregador.email)
+        cy.get('input[name="whatsapp"]').type(entregador.whatsapp)
+
+    })
+})
